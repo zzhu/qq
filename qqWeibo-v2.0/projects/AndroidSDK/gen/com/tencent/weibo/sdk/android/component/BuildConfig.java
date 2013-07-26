1b82eb31c8b5f281efe9935fbf4b6dbb5e2216b0
zengzhu <zeng.zhu@gmail.com>
2013-07-26 12:25:24 +0800

    cleanups.

--- qqWeibo-v2.0/projects/AndroidSDK/gen/com/tencent/weibo/sdk/android/component/BuildConfig.java
+++ qqWeibo-v2.0/projects/AndroidSDK/gen/com/tencent/weibo/sdk/android/component/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.tencent.weibo.sdk.android.component;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
